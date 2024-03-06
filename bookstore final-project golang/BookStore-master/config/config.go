64f4a4c93f5b3fc2ef8b8ca1d37c887aad8000fd
Armani7777 <134958781+Armani7777@users.noreply.github.com>
2024-03-06 19:43:41 +0600

    Add files via upload

---
+++ bookstore final-project golang/BookStore-master/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/g0Influencer/BookStore/app"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+var db *gorm.DB
+
+func Init() *gorm.DB {
+	dsn := "root:root@/productsdb?parseTime=true"
+	conn, err := gorm.Open("mysql", dsn)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	db = conn
+	db.AutoMigrate(&app.Product{}, &app.Cart{}, &app.Fav{})
+
+	return db
+}
+func GetDB() *gorm.DB {
+	return db
+}
---
+++ bookstore final-project golang/BookStore-master/go.mod
@@ -0,0 +1,13 @@
+module github.com/g0Influencer/BookStore
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/sessions v1.2.1
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.2 // indirect
+	golang.org/x/crypto v0.21.0
+)
